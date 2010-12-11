6e12587982696c5eba3fc6cc7f2c71090828e508
Zdenek Styblik <zdenek.styblik@gmail.com>
2010-12-11 18:42:58 +0100

    mod: config for FE ~ pgsql socket path

--- conf/config.pl
+++ conf/config.pl
@@ -2,7 +2,7 @@
 # 2010/Dec/08 @ Zdenek Styblik
 # Desc: Unified config for all Perl scripts
 
-$CFG{DB_DSN} = 'dbi:Pg:dbname=pkgs;host=/var/run/postgres/;port=5432';
+$CFG{DB_DSN} = 'dbi:Pg:dbname=pkgs;host=/tmp/;port=5432';
 $CFG{DB_USER} = 'pkgs';
 $CFG{DB_PASS} = 'swarePkgs';
 $CFG{TMPL_PATH} = '/srv/httpd/search.slackware.eu/template/';
