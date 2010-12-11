b4e0d0ce321b232f05810934465c4fe95b00de44
Zdenek Styblik <zdenek.styblik@gmail.com>
2010-12-11 17:21:47 +0100

    proper config mod!

--- conf/config.pl
+++ conf/config.pl
@@ -5,7 +5,7 @@
 $CFG{DB_DSN} = 'dbi:Pg:dbname=pkgs;host=/var/run/postgres/;port=5432';
 $CFG{DB_USER} = 'pkgs';
 $CFG{DB_PASS} = 'swarePkgs';
-$CFG{TMPL_PATH} = '/mnt/search.slackware.eu/template/';
-$CFG{SQLITE_PATH} = '/mnt/search.slackware.eu/db/';
+$CFG{TMPL_PATH} = '/srv/httpd/search.slackware.eu/template/';
+$CFG{SQLITE_PATH} = '/srv/httpd/search.slackware.eu/db/';
 
 \%CFG;
