4559e95d91462f7fd887c13e5e3aafc00a8a615c
Zdenek Styblik <zdenek.styblik@gmail.com>
2010-12-11 17:20:55 +0100

    config mod ?

--- conf/config.pl
+++ conf/config.pl
@@ -2,7 +2,7 @@
 # 2010/Dec/08 @ Zdenek Styblik
 # Desc: Unified config for all Perl scripts
 
-$CFG{DB_DSN} = 'dbi:Pg:dbname=pkgs;host=/mnt/search.slackware.eu/var/run/postgres/;port=21000';
+$CFG{DB_DSN} = 'dbi:Pg:dbname=pkgs;host=/var/run/postgres/;port=5432';
 $CFG{DB_USER} = 'pkgs';
 $CFG{DB_PASS} = 'swarePkgs';
 $CFG{TMPL_PATH} = '/mnt/search.slackware.eu/template/';
