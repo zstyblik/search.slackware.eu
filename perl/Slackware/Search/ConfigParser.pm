1c1cf8769630331bf5a8fad2cbafce9196af9b91
Zdenek Styblik <zdenek.styblik@gmail.com>
2010-12-18 20:24:24 +0100

    Simple config parser take #1

--- perl/Slackware/Search/ConfigParser.pm
+++ perl/Slackware/Search/ConfigParser.pm
@@ -3,10 +3,41 @@
 # Desc Alt: simple config file parser
 package Slackware::Search::ConfigParser;
 
+require Exporter;
+use vars qw(@ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
+@ISA = qw(Exporter);
+@EXPORT = qw();
+@EXPORT_OK = qw(_getConfig);
+
 use strict;
 use warnings;
 
-sub getConfig {
+sub _getConfig {
+	my $self = shift;
+	my $configFile = shift;
+	my %configHash;
+	return %configHash unless $configFile;
+	unless ( -e $configFile ) {
+		return %configHash;
+	} # unless -e $configFile
+	open(CFGFILE, $configFile) or return %configHash;
+	while (my $line = <CFGFILE>) {
+		next unless ($line =~ /^\$CFG{[A-Za-z0-9]+}.+=*;$/);
+		my ($left, $right) = split('@', $line);
+		my $key = substr($left, index($left, '{')+1, index($left, '}')-1);
+		$right =~ s/^\s+//g;
+		$right =~ s/\s+$//g;
+		my $value;
+		if ($right =~ /^'.*'$/ || $right =~ /^".*"$/) {
+			$right = substr($right, 1, length($right));
+			$value = chomp($right);
+		} else {
+			$value = $right;
+		}
+		$configHash{$key} = $value;
+	} # while my $line
+	close(CFGFILE);
+	return %configHash;
 } # sub getConfig
 
 1;
