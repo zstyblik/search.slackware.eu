989a7e141ad5c78a03509efb1268ddff8c896b2e
Zdenek Styblik <stybla@turnovfree.net>
2010-12-24 08:27:56 +0100

    Add: input validation for ViewSerie

--- perl/Slackware/Search/ViewSerie.pm
+++ perl/Slackware/Search/ViewSerie.pm
@@ -34,7 +34,35 @@ sub cgiapp_init {
 
 sub view_serie {
 	my $self = shift;
+	my $dbh = $self->dbh;
 	my $q = $self->query;
+	my $slackver = $q->param('slackver');
+	my $category = $q->param('category');
+	my $serie = $q->param('serie');
+	my $validSlackver = $self->_validate_slackver($slackver);
+	unless ($validSlackver) {
+		return $self->error("Slackversion is garbage.");
+	}
+	my $validCategory = $self->_validate_category($category);
+	unless ($validCategory) {
+		return $self->error("Category is garbage.");
+	}
+	my $validSerie = $self->_validate_serie($serie);
+	unless ($validSerie) {
+		return $self->error("Serie is garbage.");
+	}
+	my $idSlackver = $self->_get_slackver_id($slackver);
+	unless ($idSlackver) {
+		return $self->error("Slackversion is not in DB.");
+	}
+	my $idCategory = $self->_get_category_id($category);
+	unless ($idCategory) {
+		return $self->error("Category is not in DB.");
+	}
+	my $idSerie = $self->_get_serie_id($serie);
+	unless ($idSerie) {
+		return $self->error("Serie is not in DB.");
+	}
 	my $sql100 = sprintf("SELECT package_name FROM package WHERE id_package 
 		IN  (SELECT id_package FROM packages WHERE id_slackversion = %i AND 
 		id_category = %i AND id_serie = %i);", $idSlackver, $idCategory, 
