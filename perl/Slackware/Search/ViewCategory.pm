acf783eba98197080a9c2ae4fb94e8eb2e8f32a6
Zdenek Styblik <stybla@turnovfree.net>
2010-12-24 08:18:55 +0100

    Add: input validation

--- perl/Slackware/Search/ViewCategory.pm
+++ perl/Slackware/Search/ViewCategory.pm
@@ -34,10 +34,33 @@ sub cgiapp_init {
 
 sub view_category {
 	my $self = shift;
+	my $dbh = $self->dbh;
 	my $q = $self->query;
+	# get params
+	my $slackver = $q->param('slackver');
+	my $category = $q->param('category');
+	# validate
+	my $validSlackver = $self->_validate_slackver($slackver);
+	unless ($validSlackver) {
+		return $self->error("Slackversion is garbage");
+	}
+	my $validCategory = $self->_validate_category($category);
+	unless ($validCategory) {
+		return $self->error("Category is garbage.");
+	}
+	# look-up in DB
+	my $idSlackver = $self->_get_slackver_id($slackver);
+	unless ($idSlackver) {
+		return $self->error("Slackversion is not in DB.");
+	}
+	my $idCategory = $self->_get_category_id($category);
+	unless ($idCategory) {
+		return $self->error("Category is not in DB.");
+	}
 	my $sql100 = sprintf("SELECT serie_name FROM serie WHERE \
 		id_serie IN (SELECT id_serie FROM packages WHERE id_slackversion = %i \
 		AND id_category = %i);", $idSlackver, $idCategory);
+	my $result100 = $dbh->selectrow_arrayref();
 }
 
 1;
