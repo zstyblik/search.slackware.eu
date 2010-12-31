d2965eefc40e83b5d52770c219fd91e18b7fabc3
Zdenek Styblik <zdenek.styblik@gmail.com>
2010-12-31 22:12:44 +0100

    Mod: chlog-convert ~ add fully support for HTML creation

--- shell/changelog-convert.sh
+++ shell/changelog-convert.sh
@@ -5,20 +5,58 @@
 set -e
 set -u
 
-CHANGELOG=${1:-''}
+CFG="/srv/httpd/search.slackware.eu/conf/config.sh"
 
-if [ -z "${CHANGELOG}" ]; then
-	echo "$0 <ChangeLog.txt>"
+if [ ! -e "${CFG}" ]; then
+	echo "Config file '${CFG}' not found."
+	exit 254;
+fi
+
+source "${CFG}"
+
+ARG1=${1:-''}
+
+if [ -z "${ARG1}" ]; then
+	echo "$0 <Slackware_version>"
 	exit 1;
 fi
 
-if [ ! -e "${CHANGELOG}" ]; then
-	echo "$0: Changelog '${CHANGELOG}' doesn't seem to exist."
+echo "${ARG1}" | \
+grep -q -i -E '^slackware(64)?-(current|[0-9]+\.[0-9]+){1}$' || \
+{
+	echo "Parameter doesn't look like Slackware version to me." \
+	1>&2
+	exit 1;
+}
+
+CHANGELOGTXT="${TMPDIR}/${ARG1}/ChangeLog.txt"
+CHANGELOGDIR="${TMPDIR}/changelogs/"
+
+if [ ! -d "${CHANGELOGDIR}" ]; then
+	mkdir "${CHANGELOGDIR}";
+fi
+
+if [ ! -e "${CHANGELOGTXT}" ]; then
+	echo "$0: ChangeLog '${CHANGELOGTXT}' doesn't seem to exist."
 	exit 2;
 fi
+
+if [ ! -d "${CHANGELOGDIR}/${ARG1}" ]; then
+	mkdir "${CHANGELOGDIR}/${ARG1}";
+fi
+
+CHANGELOGTMP="${CHANGELOGDIR}/${ARG1}/ChangeLog.tmp"
 
 sed -r -e \
 	's#(^[A-Za-z]{3}[\ ]+[A-Za-z]{3}[\ ]+[0-9]{1,2}[\ ]+[0-9]{2}:[0-9]{2}:[0-9]{2}[\ ]+[A-Z]{3,4}[\ ]+[0-9]{4}$)#<h5>\1</h5><pre>#' \
 	-e 's#^\+[-]+\+#<\/pre><hr \/>#' -e 's#^[-]+$#<\/pre><hr \/>#' \
-	"${CHANGELOG}" > "${CHANGELOG}.htm" || exit 3
+	"${CHANGELOGTXT}" > "${CHANGELOGTMP}" || exit 3
+
+perl "/srv/httpd/search.slackware.eu/shell/changelog-preptemplate.pl" "${ARG1}"
+
+sed -r -e '/REPLACEME/r ${CHANGELOGTMP}' -e 's/REPLACEME//g' \
+"${CHANGELOGDIR}/${ARG1}/ChangeLog.htm.new" > \
+"${CHANGELOGDIR}/${ARG1}/ChangeLog.htm"
+
+rm -f "${CHANGELOGDIR}/${ARG1}/ChangeLog.tmp"
 
